ea3888b3fe4b7ef707814fae5d96a33ef89bd2d3
Gentilhomme <gentilhomme.thomas@gmail.com>
2019-07-15 13:24:19 +0200

    Update maintenance_mode.pl

--- maintenance_mode.pl
+++ maintenance_mode.pl
@@ -30,22 +30,24 @@ use constant {
 #====================================================
 # Globals
 #====================================================
-my $STR_Prgname   = 'maintenance_mode';
+my $STR_Prgname   = 'maintenance';
 my $STR_Version   = '1.00';
 my $STR_Edition   = '15/07/2019';
 my $MNT_QUEUE     = Thread::Queue->new();
 my $STR_NBThreads = 3;
-my $CALLBACK_ADDR = "/AXA-IM_PROD/FR1AS1IT1-0047/FR1AS1IT1-0047/maintenance_mode";
 
 #====================================================
 # Configuration File
 #====================================================
-my $CFG          = Nimbus::CFG->new($STR_Prgname.".cfg");
-my $INT_Loglevel = defined($CFG->{"setup"}->{"loglevel"}) ? $CFG->{"setup"}->{"loglevel"} : 0;
-my $INT_Logsize  = defined($CFG->{"setup"}->{"logsize"})  ? $CFG->{"setup"}->{"logsize"}  : 1024;
-my $STR_Logfile  = defined($CFG->{"setup"}->{"logfile"})  ? $CFG->{"setup"}->{"logfile"}  : $STR_Prgname.".log";
-my $STR_Login    = $CFG->{"setup"}->{"nim_login"} || "administrator";
-my $STR_Password = $CFG->{"setup"}->{"nim_password"};
+my $CFG           = Nimbus::CFG->new($STR_Prgname.".cfg");
+my $INT_Loglevel  = defined($CFG->{"setup"}->{"loglevel"}) ? $CFG->{"setup"}->{"loglevel"} : 3;
+my $INT_Logsize   = defined($CFG->{"setup"}->{"logsize"})  ? $CFG->{"setup"}->{"logsize"}  : 1024;
+my $STR_Logfile   = defined($CFG->{"setup"}->{"logfile"})  ? $CFG->{"setup"}->{"logfile"}  : $STR_Prgname.".log";
+my $STR_Login     = $CFG->{"setup"}->{"nim_login"} || "administrator";
+my $STR_Password  = $CFG->{"setup"}->{"nim_password"};
+
+my $CALLBACK_ADDR = $CFG->{"config"}->{"addr"} || "/AXA-IM_PROD/FR1AS1IT1-0047/FR1AS1IT1-0047/maintenance_mode";
+my $HTTP_ADDR     = $CFG->{"config"}->{"http_addr"} || 'http://10.130.37.121:8080';
 
 # Authenticate if nim_login & nim_password are defined
 nimLogin("$STR_Login","$STR_Password") if defined($STR_Login) && defined($STR_Password);
@@ -66,25 +68,29 @@ $SIG{__DIE__} = \&scriptDieHandler;
 sub scriptDieHandler {
     my ($err) = @_;
     print STDERR "$err\n";
-    nimLog(0, "$err");s
+    nimLog(0, "$err");
     exit(1);
 }
 
-my $serverThreads = sub {
+my $thrHandler;
+$thrHandler = sub {
     nimLog(3, "Thread started!");
     while ( defined ( my $options = $MNT_QUEUE->dequeue() ) ) {
         my $Server = $options->{server};
         my $State = $options->{state};
-        nimLog(3, "In thread: $Server with state $State");
 
         # Retrieve Server Master Id
-        my $MasterDeviceID = src::utils::getMasterDeviceId($Server);
+        my $MasterDeviceID = src::utils::getMasterDeviceId($HTTP_ADDR, $Server);
+        if (!defined($MasterDeviceID)) {
+            next;
+        }
+        nimLog(3, "[$Server] MasterDeviceID => $MasterDeviceID");
 
         my $DATA = Nimbus::PDS->new();
         $DATA->put("scheduleId", "273", PDS_PCH);
         $DATA->put("csIds", "$MasterDeviceID", PDS_PCH);
 
-        my $callbackName = $State == MANAGED ? 
+        my $callbackName = $State eq MANAGED ? 
             "remove_computer_systems_from_schedule" :
             "add_computer_systems_to_schedule";
         my ($RC, $RES) = nimNamedRequest($CALLBACK_ADDR, $callbackName, $DATA->data);
@@ -103,14 +109,14 @@ my $serverThreads = sub {
 
 # Wait for group threads
 my @thr = map {
-    threads->create(\&$serverThreads);
+    threads->create(\&$thrHandler);
 } 1..$STR_NBThreads;
 $_->detach() for @thr;
 
 # CALLBACK Declarations
 sub managed {
     my ($hMsg, $serverName) = @_;
-    nimLog(3, "(managed) Handle server: $serverName");
+    nimLog(3, "managed callback triggered with server '$serverName'");
     $MNT_QUEUE->enqueue({
         server => $serverName, state => MANAGED
     });
@@ -120,7 +126,7 @@ sub managed {
 
 sub unmanaged {
     my ($hMsg, $serverName) = @_;
-    nimLog(3, "(unmanaged) Handle server: $serverName");
+    nimLog(3, "unmanaged callback triggered with server '$serverName'");
     $MNT_QUEUE->enqueue({
         server => $serverName, state => UNMANAGED
     });
@@ -132,7 +138,6 @@ sub unmanaged {
 sub timeout {
     # Do something, normally you would put your monitoring code
     # here, checking the elapsed time.
-    print("timeout triggered!");
 }
 
 sub restart {
@@ -146,6 +151,6 @@ if ($sess->server (NIMPORT_ANY,\&timeout,\&restart)==0) {
     $sess->addCallback("managed", "serverName");
     $sess->addCallback("unmanaged", "serverName");
 
-    nimLog(3, "Dispatch timeout callback at 1,000ms");
-    $sess->dispatch(1000);
+    nimLog(3, "Dispatch timeout callback at 10,000ms");
+    $sess->dispatch(10000);
 }
